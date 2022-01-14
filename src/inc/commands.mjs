e00380166530ed00822687658cf09398f824743e
Nauris Pūķis <hi@pyronaur.com>
2022-01-14 07:32:46 +0200

    info/utility -> npaths

--- src/inc/commands.mjs
+++ src/inc/commands.mjs
@@ -1,119 +1,122 @@
-import utility from "./info.mjs";
+import npaths from "./npaths.mjs";
 import { confirm } from "./helpers.mjs";
 
 async function relink() {
-  console.log("Re-creating script links");
+	console.log("Re-creating script links");
+	
+	const bin_directory = `${npaths.bindDir}`;
+	await fs.ensureDir(bin_directory);
 
-  const bin_directory = `${utility.bindDir}`;
-  await fs.ensureDir(bin_directory);
+	for (const script of await npaths.list()) {
+		const { bin, file } = npaths.paths(script);
 
-  for (const script of await utility.list()) {
-    const { bin, file } = utility.paths(script);
-
-    if (argv.force === true && (await fs.pathExists(bin)) === true) {
-      console.log(`Removing ${bin}`);
-      await $`rm ${bin}`;
-    }
-
-    if (false === (await fs.pathExists(bin))) {
-      console.log(`Linking ${bin}`);
-      await $`ln ${utility.wildcard} ${bin}`;
-    }
-  }
+		if( argv.force === true && ( await fs.pathExists(bin) ) === true ) {
+			console.log(`Removing ${bin}`)
+			await $`rm ${bin}`;
+		}
+		
+		if ( false === await fs.pathExists(bin) ) {
+			console.log(`Linking ${bin}`);
+			await $`ln ${npaths.wildcard} ${bin}`;
+		}
+	}
 }
 
 async function cleanup() {
-  console.log("Cleaning up the bins...");
+	console.log("Cleaning up the bins...");
 
-  const bins = await utility.bins();
-  const utilities = await utility.list();
+	const bins = await npaths.bins();
+	const utilities = await npaths.list();
 
-  for (const bin of bins) {
-    const binName = path.basename(bin);
-    if (!utilities.includes(binName)) {
-      if (await confirm(`Delete ${binName}?`, "y")) {
-        await $`rm ${bin}`;
-      }
-    }
-  }
+	for (const bin of bins) {
+		const binName = path.basename(bin);
+		if (!utilities.includes(binName)) {
+			if (await confirm(`Delete ${binName}?`, "y")) {
+				await $`rm ${bin}`;
+			}
+		}
+	}
 
-  console.log("No more bins left to clean up!");
+	console.log("No more bins left to clean up!");
 }
 
 async function create({ slug, file, bin }) {
-  if (!slug) {
-    console.log("Specify the utility slug.\nutils create <utility-name>");
-    return false;
-  }
+	if (!slug) {
+		console.log("Specify the utility slug.\nutils create <utility-name>");
+		return false;
+	}
 
-  if (await fs.pathExists(file)) {
-    console.log("Already exists");
-    return true;
-  }
+	if (await fs.pathExists(file)) {
+		console.log("Already exists");
+		return true;
+	}
 
-  const v = $.verbose;
-  $.verbose = false;
+	const v = $.verbose;
+	$.verbose = false;
 
-  const commandExists = await nothrow($`which ${slug}`);
+	const commandExists = await nothrow($`which ${slug}`);
 
-  if (commandExists.exitCode !== 1) {
-    const alias = (await $`which ${slug}`).stdout;
-    console.log(`Command "${slug}" is already aliased to "${alias.trim()}"\n`);
-    process.exit();
-  }
-  $.verbose = v;
+	if (commandExists.exitCode !== 1) {
+		const alias = (await $`which ${slug}`).stdout;
+		console.log(
+			`Command "${slug}" is already aliased to "${alias.trim()}"\n`
+		);
+		process.exit();
+	}
+	$.verbose = v;
 
-  if (false === (await confirm(`Create new utility "${slug}"?`))) {
-    console.log("Ok, bye.");
-    process.exit();
-  }
 
-  console.log("Creating a new utility script: " + slug);
+	if (false === await confirm(`Create new utility "${slug}"?`)) {
+		console.log("Ok, bye.");
+		process.exit();
+	}
+	
+	console.log("Creating a new utility script: " + slug);
 
-  await $`echo '#!/usr/bin/env zx' >> ${file}`;
-  await $`chmod a+x ${file}`;
-  await $`ln ${file} ${bin}`;
+	await $`echo '#!/usr/bin/env zx' >> ${file}`;
+	await $`chmod a+x ${file}`;
+	await $`ln ${file} ${bin}`;
 
-  await $`code ${file}`;
+	await $`code ${file}`;
 }
 
 async function edit({ slug, file, bin }) {
-  if (slug) {
-    if (await fs.pathExists(file)) {
-      await $`code ${file}`;
-    } else {
-      console.log(`Utility "${slug}" doesn't exist`);
-      await create({ slug, file, bin });
-    }
-    return;
-  }
+	if (slug) {
+		if (await fs.pathExists(file)) {
+			await $`code ${file}`;
+		} else {
+			console.log(`Utility "${slug}" doesn't exist`);
+			await create({ slug, file, bin });
+		}
+		return;
+	}
 
-  await $`code ${utility.directory}`;
+	await $`code ${npaths.directory}`;
 }
 
 async function remove({ slug }) {
-  if (!slug) {
-    console.log("Specify the utility slug.\nutils create <utility-name>");
-    return false;
-  }
+	if (!slug) {
+		console.log("Specify the utility slug.\nutils create <utility-name>");
+		return false;
+	}
 
-  if (false === (await confirm(`Delete utility "${slug}"?`))) {
-    return false;
-  }
+	if ( false === await confirm(`Delete utility "${slug}"?`)) {
+		return false;
+	}
 
-  const { file, bin } = utility.paths(slug);
-  await $`rm ${file}`;
-  await $`rm ${bin}`;
+	const { file, bin } = npaths.paths(slug);
+	await $`rm ${file}`;
+	await $`rm ${bin}`;
 }
 
 async function list() {
-  console.log(
-    " " +
-      (await utility.list())
-        .map((name) => `\n - ${name}`)
-        .join("")
-        .trim()
-  );
+	console.log(
+		" " +
+			(await npaths.list())
+				.map((name) => `\n - ${name}`)
+				.join("")
+				.trim()
+	);
 }
 
 export { list, create, edit, remove, relink, cleanup };
