3f578f7b76b480a1d7aa35a375c2120a26f337ba
Nauris Pūķis <hi@pyronaur.com>
2022-01-20 22:28:00 +0200

    Wrong variable name, again.
    Typescript wouldn't have saved me anyway.

--- src/inc/config.mjs
+++ src/inc/config.mjs
@@ -67,8 +67,8 @@ export async function addSources() {
 
 	const addedName = path.basename(pathToAdd)
 	const addedSymlink = `${paths.sources}/${addedName}`
-		
-	if (defaultSource !== sourcePath && !fs.pathExistsSync(addedSymlink)) {
+
+	if (defaultSource !== pathToAdd && !fs.pathExistsSync(addedSymlink)) {
 		await $`ln -s ${pathToAdd} ${addedSymlink}`;
 	}
 	update("sources", [...sources]);
