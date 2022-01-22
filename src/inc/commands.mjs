3561f86dbe06e185db930e35b8d03f9bc6300a4b
Nauris Pūķis <hi@pyronaur.com>
2022-01-22 16:30:21 +0200

    Update message

--- src/inc/commands.mjs
+++ src/inc/commands.mjs
@@ -15,7 +15,7 @@ info.link = {
 async function link() {
 
 	if (await relinkBins()) {
-		console.log("Creating executable links to your scripts.");
+		console.log("\nDone!");
 	} else {
 		console.log("All executables are already linked.")
 		console.log(chalk.gray("Use the --force if you must"));
